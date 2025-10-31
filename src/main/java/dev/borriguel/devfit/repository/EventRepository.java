0180076d088a0d0445a622f56e7c8e618f71ba0a
Borriguel <rodolpho-henrique@hotmail.com.br>
2025-10-31 06:10:27 -0300

    Add pagination support to EventRepository with findByUnit_Id method

--- src/main/java/dev/borriguel/devfit/repository/EventRepository.java
+++ src/main/java/dev/borriguel/devfit/repository/EventRepository.java
@@ -1,9 +1,12 @@
 package dev.borriguel.devfit.repository;
 
 import dev.borriguel.devfit.model.Event;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface EventRepository extends JpaRepository<Event, Long> {
+    Page<Event> findByUnit_Id(Long id, Pageable pageable);
 }
